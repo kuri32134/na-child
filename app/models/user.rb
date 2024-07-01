99044cbcc9f60cbd8d6e8e419668b7fcc60fff2a
kuri32134 <161860576+kuri32134@users.noreply.github.com>
2024-07-01 05:04:58 +0000

    [add]bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,13 @@ class User < ApplicationRecord
   def full_name_kana
     self.last_name_kana + " " + self.first_name_kana
   end
+  
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 
 end
