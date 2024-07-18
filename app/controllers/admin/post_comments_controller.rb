764b559a0b9ed9d128378ccd2b9f396359ccc73b
kuri32134 <161860576+kuri32134@users.noreply.github.com>
2024-07-18 08:15:01 +0000

    [Fix]adminコメント削除後の遷移先

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -9,6 +9,6 @@ class Admin::PostCommentsController < ApplicationController
   def destroy
     @post_comment = PostComment.find(params[:id])
     @post_comment.destroy
-    redirect_to admin_post_path(@post_comment.post.id)
+    redirect_to admin_post_comments_path
   end
 end
